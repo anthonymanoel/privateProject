05a6cec7d360cecd99dbe232c0dd426ed069d78d
Anthony <anthonymanoel12@gmail.com>
2022-06-08 18:57:21 -0300

    release lastest updates and turn front at the main page

--- project to portfolio/assets/js/index.js
+++ project to portfolio/assets/js/index.js
@@ -1,14 +1,7 @@
-const navSlide = () => {
-    const burguer = document.querySelector('.burguer');
-        const nav = document.querySelector('.nav-navbar');
-            const navbar = document.querySelectorAll('.nav-navbar li')
-    burguer.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-    });
-    navbar.forEach((link, index) =>{
-        console.log(index)
-        link.style.animation = `navlinkFade 0.5s ease forwards ${index / 5 + 0.2}s`;
-    })
+function fade(ele) {
+    ele.fadeIn(1000).delay(3000).fadeOut(1000, function () {
+        var $next = $(this).next('.quote');
+        fade($next.length > 0 ? $next : $(this).parent().children().first());
+   });
 }
-
-navSlide();
+fade($('.quoteLoop > .quote').first());
